647d1d3c3cbb770994cc00dcf752d4d25c44f750
Salaheddine.LAHMAM <salahedd21@gmail.com>
2024-10-14 20:04:23 +0100

    Auto-generated commit

---
+++ chunk-LOVW4PRI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-PXS37LCD.js";import"./chunk-QOTJRCNM.js";export{a as ProductModule};
---
+++ chunk-S6KI33FO.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-33BNBKYR.js";import"./chunk-QOTJRCNM.js";export{a as CartModule};
