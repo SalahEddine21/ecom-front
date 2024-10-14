040010e00736b29ee5df78cfa2ae4ffe36b1d6b3
Salaheddine.LAHMAM <salahedd21@gmail.com>
2024-10-14 19:51:16 +0100

    update env files

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -2,19 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl; 
 
   constructor(private readonly httpClient : HttpClient) { }
 
   getProducts() : Observable<Product[]>{
-    return this.httpClient.get<Product[]>('/api/products');
+    return this.httpClient.get<Product[]>(`${this.apiUrl}/products`);
   }
 
   getCategories() : Observable<String[]> {
-    return this.httpClient.get<String[]>('/api/products/categories');
+    return this.httpClient.get<String[]>(`${this.apiUrl}/products/categories`);
   }
 }
